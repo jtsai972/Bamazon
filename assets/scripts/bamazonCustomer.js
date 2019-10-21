d08ebddc79a7a6d2c3d23faf8c4b497499ead47c
J Tsai <jtsai972@gmail.com>
2019-10-20 22:02:59 -0500

    set up the function to query servers

--- assets/scripts/bamazonCustomer.js
+++ assets/scripts/bamazonCustomer.js
@@ -4,7 +4,7 @@ console.log("Entering customer perspective");
 const inquirer = require("inquirer");
 const server = require("./serverAccess");
 
-
+let first, last;
 
 let customer = function(){
     console.log("Hi, I'm a customer!");
@@ -13,25 +13,64 @@ let customer = function(){
 
 module.exports = customer;
 
+customer();
 
 //start();
 function start() {
     server.openDB().query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.table(res);
+
+        let test = {}
+        for(let i = 0; i < res.length; i++) {
+            test[res[i].id] = {
+                name: res[i].name,
+                department: res[i].department,
+                price: res[i].price = res[i].price.toFixed(2),
+                quantity: res[i].quantity
+            }
+        }
+        console.table(test);
+
+        first = res[0].id;
+        last = res[res.length-1].id;
+
+        console.log(res.length);
 
         inquirer.prompt([
             {
                 type: "number",
-                message:"What is the ID of the product you want to buy?",
-                name: "id"
+                message:"What is the index of the product you want to buy?",
+                name: "id",
+                validate: numVal
             },
             {
                 type: "number",
                 message: "How many do you wish to purchase?",
-                name: "quantity"
+                name: "quantity",
+                validate: isNum
             }
         ]).then(answer => {
+            let id = answer.id,
+                num = answer.quantity,
+                item = res[id-1];
+
+            console.log(" ");
+
+            //Item and current quantity
+            console.log(`Item: ${item.name} \nQuantity: ${item.quantity}`);
+
+            console.log(" ");
+
+            if (item.quantity < num) {
+                console.log( 
+                    item.quantity === 0 ? 
+                        "Sorry, we are out of stock!" :
+                        "Sorry, we don't have that many left!"
+                )
+                server.closeDB();
+            } else {
+                purchase()
+            }
             
 
             server.closeDB();
@@ -41,3 +80,23 @@ function start() {
 }
 
 // customer();
+
+let numVal = function(answer) {
+    if(typeof answer !== "number" || typeof answer === NaN) {
+        return "Sorry, that is not a valid product id!";
+    } else if ( answer < first || answer > last) {
+        return "Sorry, that is not a valid product id!";
+    } else {
+        return true;
+    }
+}
+
+let isNum = function(answer) {
+    if (typeof answer !== "number" || typeof answer === NaN) {
+        return "Sorry, that is not a valid product id!";
+    } else if (answer < 0) {
+        return "Sorry, that is not a valid product id!";
+    } else {
+        return true;
+    }
+}

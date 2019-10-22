5f48197bd0422c8130c1883d4707f35a921080ae
J Tsai <jtsai972@gmail.com>
2019-10-22 00:45:15 -0500

    Fixed inquirer prompt  for handling NaN

--- assets/scripts/bamazonCustomer.js
+++ assets/scripts/bamazonCustomer.js
@@ -1,19 +1,15 @@
-console.log("Entering customer perspective");
+// console.log("Entering customer perspective");
 
 // * setting up requirements
 const inquirer = require("inquirer");
 const server = require("./serverAccess");
+const dDiv = "------------------------------------------------------",
+    eDiv = "======================================================";
 
 let first, last;
 
 let customer = function(){
-    console.log("Hi, I'm a customer!");
-
-    // server.openDB().connect(function(err) {
-    //     if(err) throw err;
-    //     console.log("connected as id " + server.openDB().threadId + "\n");
-    // })
-
+    //console.log("Hi, I'm a customer!");
     start();
 }
 
@@ -33,70 +29,104 @@ function start() {
         first = res[0].id;
         last = res[res.length-1].id;
 
-        inquirer.prompt([
-            {
-                type: "number",
-                message:"What is the id (not index) of the product you want to buy?",
-                name: "id",
-                validate: numVal
-            },
-            {
-                type: "number",
-                message: "How many do you wish to purchase?",
-                name: "quantity",
-                validate: isNum
-            }
-        ]).then(answer => {
-            let id = answer.id,
-                num = answer.quantity,
-                item = res[id-1];
-
-            console.log(" ");
-
-            //Item and current quantity
-            //console.log(`Item: ${item.name} \nQuantity: ${item.quantity}`);
-
-            //console.log(" ");
-
-            if (item.quantity < num) {
-                console.log( 
-                    item.quantity === 0 ? 
-                        "Sorry, we are out of stock!" :
-                        "Sorry, we don't have that many left!"
-                )
-                server.closeDB();
-            } else {
-                let receipt = [
-                    "Item: " + item.name,
-                    "Quantity: " + num,
-                    "Price: " + item.price,
-                    "Total: " + (item.price * num).toFixed(2),
-                    "\n"
-                ].join("\n");
-                console.log(receipt);
-                purchase(id, item.quantity, num);
-            }
-        })
+        purchasePrompt(res);
         
     });
     //console.log(query.sql);
     query
 }
 
+function purchasePrompt(res) {
+    inquirer.prompt([
+        {
+            type: "number",
+            message:"What is the id (not index) of the product you want to buy?",
+            name: "id",
+            validate: numVal
+        },
+        {
+            type: "number",
+            message: "How many do you wish to purchase?",
+            name: "quantity",
+            validate: isNum
+        }
+    ]).then(answer => {
+        let id = answer.id,
+            num = answer.quantity,
+            item = res[id-1];
+
+        console.log(" ");
+
+        // * Item and current quantity
+        //console.log(`Item: ${item.name} \nQuantity: ${item.quantity}\n`);
+
+        if (item.quantity < num) {
+            console.log( 
+                item.quantity === 0 ? 
+                    "Sorry, we are out of stock!" :
+                    "Sorry, we don't have that many left!"
+            )
+            newPurchase();
+        } else {
+            let receipt = [
+                eDiv,
+                "Your order:",
+                dDiv,
+                "    Purchase: " + item.name,
+                "    Quantity: " + num,
+                "    Price: " + item.price,
+                "    Total: " + (item.price * num).toFixed(2),
+                dDiv,
+                "Thank you for your purchase!",
+                eDiv + "\n",
+            ].join("\n");
+
+            console.log(receipt);
+
+            purchasing(item, num);
+        }
+    })
+    
+    
+}
+
+/**-----------------------------------------------
+ * * Validation Stuff down here
+ * ----------------------------------------------- */
+function check(chk, answer) {
+    //console.log("check " + answer);
+
+    let reg = /^\d+$/;
+    let checkNum = reg.test(answer);
+    let msg = "Sorry, that is not a valid product id!";
+
+    //console.log("Testing regex: " + reg.test(answer));
+    return !checkNum || chk ? msg : true;
+}
+function numVal(answer) {
+    let chk =  answer < first || answer > last;
+    return check(chk, answer);
+}
+
+function isNum(answer) {
+    let chk = answer < 0;
+    return check(chk, answer);
+}
+
 /**-----------------------------------------------
  * * Purchasing Stuff down here
  * ----------------------------------------------- */
-function purchase(id, stock, purchase) {
+function purchasing(item, purchase) {
     //console.log("Purchasing");
 
     var query = server.openDB().query(
         "UPDATE products SET ? WHERE ?",
-        [{quantity: (stock-purchase)},{id: id}],
+        [{quantity: (item.quantity-purchase)},{id: item.id}],
         function(err, res) {
             if (err) throw err;
             console.log(res.affectedRows + " products updated!\n");
 
-            showProducts()
+            newPurchase()
         }
     )
     //console.log(query.sql);
@@ -106,46 +136,27 @@ function purchase(id, stock, purchase) {
 /**-----------------------------------------------
  * * Displaying Products down here
  * ----------------------------------------------- */
-function showProducts() {
-    var query = server.openDB().query(
-        "SELECT * FROM products", function(err, res) {
-            if(err) throw err;
-
-            for(let i = 0; i < res.length; i++) {
-                res[i].price = res[i].price.toFixed(2);
-            }
-
-            console.table(res);
+function newPurchase() {
+    console.log("\n" + eDiv + "\n");
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Would you like to make another purchase?",
+            choices: ["yes", "no"],
+            name: "continue"
+        }
+    ]).then(answer => {
+        console.log("\n" + eDiv + "\n");
+        if(answer.continue === "yes") {
+            start();
+        } else {
+            console.log("\nThank you for shopping with Bamazon!");
+            console.log("Have a nice day!\n");
             server.closeDB();
         }
-    )
-    //console.log(query.sql);
-    query
+    })
 }
 
-
-/**-----------------------------------------------
- * * Validation Stuff down here
- * ----------------------------------------------- */
-let numVal = function(answer) {
-    if(typeof answer !== "number" || typeof answer === NaN) {
-        return "Sorry, that is not a valid product id!";
-    } else if ( answer < first || answer > last) {
-        return "Sorry, that is not a valid product id!";
-    } else {
-        return true;
-    }
-}
-
-let isNum = function(answer) {
-    if (typeof answer !== "number" || typeof answer === NaN) {
-        return "Sorry, that is not a valid product id!";
-    } else if (answer < 0) {
-        return "Sorry, that is not a valid product id!";
-    } else {
-        return true;
-    }
-}
 
 /**-----------------------------------------------
  * * Testing Stuff down here

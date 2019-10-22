01901ea05986e2bb42d33bf7e95ec7d68611bb4a
J Tsai <jtsai972@gmail.com>
2019-10-22 17:23:13 -0500

    changing seeds for the manager stuff

--- assets/database/seeds-products.sql
+++ assets/database/seeds-products.sql
@@ -7,7 +7,7 @@ VALUES
     -- Item 01
     ( "Apple Barrel Acrylics - Assorted Colors (18 pack)", "Arts and Crafts", 17.99, 53 ), 
     -- Item 02
-    ( "BamazonBasics Blue and Yellow Microfiber Cleaning Cloth, 24-Pack", "Automotive", 12.81, 0 ), 
+    ( "BamazonBasics Blue and Yellow Microfiber Cleaning Cloth, 24-Pack", "Automotive", 12.81, 2 ), 
     -- Item 03
     ( "Q-tips Cotton Swabs, 750 ct", "Beauty and Personal Care", 4.64, 87 ),
     -- Item 04
@@ -19,7 +19,7 @@ VALUES
     -- Item 07
     ( "Mellanni Sheet Set - 3 Piece (Twin, Baby Blue)", "Home and Kitchen", 27.00, 45 ), 
     -- Item 08
-    ( "Hydro Flask Wide Mouth Water Bottle, Straw Lid - Multiple Sizes & Colors", "Sports and Outdoors", 44.95, 7 ), 
+    ( "Hydro Flask Wide Mouth Water Bottle, Straw Lid - Multiple Sizes & Colors", "Sports and Outdoors", 44.95, 4 ), 
     -- Item 09
     ( "Affresh Washer Machine Cleaner, 6-Tablets, 8.4 oz", "Tools & Home Improvement", 11.99, 75 ), 
     -- Item 10
